56007c46ad4ebdf4e1d908ae6f10285365492fd7
Armand Philippot <59021693+ArmandPhilippot@users.noreply.github.com>
2020-06-07 18:34:29 +0200

    fix: sticky post & pagination if no static page
    When no static page is defined, the front-page.php file is used to list
    the posts. The sticky posts were not displayed correctly. The
    pagination was missing.
    Now the display is correct whether a static page is defined as a front
    page or not.

--- front-page.php
+++ front-page.php
@@ -9,10 +9,47 @@
 get_header(); ?>
 <main id="main" class="site-main content-area">
 	<?php
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post();
-			get_template_part( 'template-parts/page/content', '' );
+	if ( is_home() ) {
+		if ( have_posts() ) {
+			?>
+			<article itemtype="http://schema.org/Article" itemscope="itemscope">
+				<header class="entry-header">
+					<?php if ( ! minimalist_is_frontpage() ) { ?>
+						<h1 class="entry-title"><?php single_post_title(); ?>
+						</h1>
+					<?php } else { ?>
+						<h2 class="entry-title"><?php esc_html_e( 'Posts', 'Minimalist' ); ?>
+						</h2>
+					<?php } ?>
+				</header><!-- .entry-header -->
+				<div class="entry-content">
+					<div class="entries-list">
+						<?php
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'template-parts/post/content', get_post_format() );
+						}
+						the_posts_pagination(
+							array(
+								'mid_size'  => 2,
+								'prev_text' => __( 'Previous', 'Minimalist' ),
+								'next_text' => __( 'Next', 'Minimalist' ),
+							)
+						);
+						?>
+					</div><!-- .entries-list -->
+				</div><!-- .entry-content -->
+			</article>
+			<?php
+		} else {
+			get_template_part( 'template-parts/post/content', 'none' );
+		}
+	} else {
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/page/content', '' );
+			}
 		}
 	}
 	?>
